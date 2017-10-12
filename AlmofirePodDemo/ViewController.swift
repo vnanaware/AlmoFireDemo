00553d09c9e7cf90bac194b23f4448b74ba4979c
Admin <admin@vishal.local>
2017-10-12 11:54:49 +0530

    Initial Commit

---
+++ AlmofirePodDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlmofirePodDemo
+//
+//  Created by Admin on 12/10/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
